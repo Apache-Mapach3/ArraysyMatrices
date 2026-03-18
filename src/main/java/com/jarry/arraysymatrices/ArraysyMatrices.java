5abe46a3b6123d36ba2327decc6df919d8f5c789
Jose Rivera <jriverar1@unicartagena.edu.co>
2026-03-18 12:36:04 -0500

    chore: commit inicial estructura en netbeans

---
+++ src/main/java/com/jarry/arraysymatrices/ArraysyMatrices.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jarry.arraysymatrices;
+
+/**
+ *
+ * @author Ghost
+ */
+public class ArraysyMatrices {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
