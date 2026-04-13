d11d680e1f3dde46433ae56b96e890373743da0b
Jose Rivera <jriverar1@unicartagena.edu.co>
2026-04-13 07:43:54 -0500

    feat(Matrices):Agrego el codigo de las matrices en java

--- src/main/java/com/jarry/arraysymatrices/Matrices.java
+++ src/main/java/com/jarry/arraysymatrices/Matrices.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.jarry.arraysymatrices;
 
-/**
- *
- * @author Ghost
- */
 public class Matrices {
-    
+
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        imprimir(matriz);
+        imprimirPorColumnas(matriz);
+
+        System.out.println("Suma: " + sumar(matriz));
+
+        intercambiarFilas(matriz);
+        imprimir(matriz);
+    }
+
+    static void imprimir(int[][] m) {
+        for (int[] fila : m) {
+            for (int valor : fila) {
+                System.out.print(valor + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static void imprimirPorColumnas(int[][] m) {
+        for (int j = 0; j < m[0].length; j++) {
+            for (int i = 0; i < m.length; i++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int sumar(int[][] m) {
+        int suma = 0;
+        for (int[] fila : m) {
+            for (int valor : fila) {
+                suma += valor;
+            }
+        }
+        return suma;
+    }
+
+    static void intercambiarFilas(int[][] m) {
+        int[] temp = m[0];
+        m[0] = m[m.length - 1];
+        m[m.length - 1] = temp;
+    }
 }
